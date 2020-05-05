dfa68d48d70f3b264dffb0085fc39d0f2148680f
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-05-05 11:02:48 +0200

    Konstruktor-Methoden für die Baum Klasse erstellt

--- src/binary_tree/tree.java
+++ src/binary_tree/tree.java
@@ -1,5 +1,33 @@
 package binary_tree;
 
 public class tree {
-    // TODO: Klasse implementieren
+
+    node root;
+
+    node leftSub;
+
+    node rightSub;
+
+    /**
+     * Konstruktor für einen Baum ohne childs
+     * @param pRoot - Wurzel-node
+     */
+    tree(node pRoot) {
+        this.root = pRoot;
+        this.leftSub = null;
+        this.rightSub = null;
+    }
+
+    /**
+     * Konstruktor für einen Baum mit childs
+     * @param pRoot - Wurzel-node
+     * @param pLeftSub - "left-child"
+     * @param pRightSub - "right-child"
+     */
+    tree (node pRoot, node pLeftSub, node pRightSub) {
+        this.root = pRoot;
+        this.leftSub = pLeftSub;
+        this.rightSub = pRightSub;
+    }
+
 }
