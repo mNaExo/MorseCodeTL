16944c2234567b2aa95971359b4853615a61424e
Maximilian Herzig <maximilian-herzig@hotmail.com>
2020-05-06 17:40:25 +0200

    morse_key ist abgetippt, scheißarbeit...

--- src/morse/morse_key.java
+++ src/morse/morse_key.java
@@ -9,4 +9,34 @@ public enum morse_key {
     /*
     TODO: Alle Buchstaben mit entsprechendem Code eintippen
      */
+    a(".-"),
+    b("-..."),
+    c("-.-"),
+    d("-.."),
+    e("."),
+    f("..-."),
+    g("--."),
+    h("...."),
+    i(".."),
+    j(".---"),
+    k("-.-"),
+    l(".-.."),
+    m("--"),
+    n("-."),
+    o("---"),
+    p(".--."),
+    q("--.-"),
+    r(".-."),
+    s("..."),
+    t("-"),
+    u("..-"),
+    v("...-"),
+    w(".--"),
+    x("-..-"),
+    y("-.--"),
+    z("--.");
+
+    public final String morseCode;
+
+    morse_key(String pMorseCode) {this.morseCode = pMorseCode;}
 }
